8666aa80fd10c31658503f48684b594817932f55
blee1710 <s3707616@student.rmit.edu.au>
2020-08-28 11:28:04 +1000

    Added show, connected hardcoded routes to db

--- app.js
+++ app.js
@@ -27,23 +27,25 @@ mongoose.connect(connectionString, {
 // SCHEMA SETUP
 var webseriesSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Webseries = mongoose.model("Webseries", webseriesSchema);
 
-Webseries.create(
-    {name: "The Wandering Inn", 
-    image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1533270114l/41033158._SY475_.jpg"
-    }, (err, webseries) => {
-        if(err){
-            console.log(err);
-        } else {
-            console.log("New Webseries!")
-            console.log(webseries)
-        }
-    }
-    );
+// Webseries.create(
+//     {name: "The Wandering Inn", 
+//     image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1533270114l/41033158._SY475_.jpg",
+//     description: "Really long series, a Banger."
+//     }, (err, webseries) => {
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("New Webseries!")
+//             console.log(webseries)
+//         }
+//     }
+//     );
 
 // END SCHEMA SETUP
 
@@ -62,26 +64,30 @@ Webseries.create(
 //     {name: "Worm", image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1519662877i/18713259._UY437_SS437_.jpg"}
 // ];
 
+// GETS LANDING PAGE
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+// SHOWS ALL WEBSERIES
 app.get("/webseries", function(req, res){
     Webseries.find({}, (err, webseries) => {
         if (err){
             console.log(err);
         } else {
-            res.render("webseries", {webseries:webseries})
+            res.render("index", {webseries:webseries})
         }
     }
     )
 })
     // res.render("webseries", {webseries:webseries});
 
+// CREATES NEW WEBSERIES
 app.post("/webseries", function(req,res){
     var name = req.body.name ;
     var image = req.body.image;
-    var newWebseries = {name: name, image: image};
+    var desc = req.body.description;
+    var newWebseries = {name: name, image: image, description: desc};
     Webseries.create(newWebseries, (err, newlyCreated) =>{
         if(err){
             console.log(err);
@@ -91,12 +97,21 @@ app.post("/webseries", function(req,res){
     })
 })
 
+// SHOWS FORM TO CREATE NEW WEBSERIES
 app.get("/webseries/new", function(req,res) {
     res.render("new.ejs");
 })
 
+
+// SHOWS INFO ABOUT ONE WEBSERIES
 app.get("/webseries/:id", function(req,res){
-    
+    Webseries.findById(req.params.id, (err, foundWebseries) =>{
+        if(err){
+            console.log(err);
+        } else {
+            res.render("show", {webseries:foundWebseries});
+        }
+    });
 })
 
 app.listen(port, () => {
