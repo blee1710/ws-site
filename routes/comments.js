47b0a61be725288798f9017164d190a194e22267
blee1710 <s3707616@student.rmit.edu.au>
2020-08-29 12:54:53 +1000

    Added delete functionality for comments

--- routes/comments.js
+++ routes/comments.js
@@ -60,6 +60,17 @@ router.put("/:comment_id", (req, res) => {
     })
 })
 
+// DESTROY COMMENT ROUTE
+router.delete("/:comment_id", (req, res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, (err) =>{
+        if(err){
+            res.redirect("back")
+        } else {
+            res.redirect("/webseries/" + req.params.id)
+        }
+    })
+})
+
 // Middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
